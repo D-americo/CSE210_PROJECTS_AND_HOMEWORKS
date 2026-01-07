98c3a860f89b4e8ce682524eea7184f60f1bb268
Daniel Américo <danielrenatodanger@gmail.com>
2026-01-06 21:52:44 -0300

    exercise 2 finished i i was able to add + and - to A, B and C scores

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string gradefromuser = Console.ReadLine();
+
+        int grade = int.Parse(gradefromuser);
+
+        if (grade > 90)
+        {
+            Console.WriteLine ("Congratulations you have passed of year with an A Score");
+            {
+            if (grade >= 97)
+            {
+                Console.WriteLine ("And exactly you got a A+");
+            }
+            else if (grade < 93 && grade >=90)
+            {
+                Console.WriteLine ("And exactly you got a A-"); 
+            }
+            else
+                {
+                Console.WriteLine ("");
+                }
+            }
+        }
+
+        else if (grade > 80)
+        {
+            Console.WriteLine ("Congratulations you have passed of year with a B Score");
+            {
+                if (grade >= 87)
+            {
+                Console.WriteLine ("And exactly you got a B+");
+            }
+            else if (grade < 83 && grade >=80)
+            {
+                Console.WriteLine ("And exactly you got a B-"); 
+            }
+            else
+                {
+                 Console.WriteLine ("");
+                }
+            }
+        }
+
+        else if (grade >= 70)
+        {
+            Console.WriteLine ("Congratulations you have passed of year with a c Score");
+            {
+                if (grade >= 77)
+            {
+                Console.WriteLine ("And exactly you got a C+");
+            }
+            else if (grade < 73 && grade >=70)
+            {
+             Console.WriteLine ("And exactly you got a C-"); 
+            }
+            else
+                {
+                Console.WriteLine ("");
+                }
+            }
+        }
+
+        else 
+        {
+            Console.WriteLine ("you have not passed of year because your grade is D or even worse F, study more and search for help. ");
+        }
     }
+       
 }

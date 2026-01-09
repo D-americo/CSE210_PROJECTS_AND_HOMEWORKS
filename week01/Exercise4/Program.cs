f3df32892e3a79a4a52f96bf625ab2b51cfe5092
Daniel Américo <danielrenatodanger@gmail.com>
2026-01-08 23:03:10 -0300

    finished execise 4!!!

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+      List<int> numbers = new List<int>();
+      
+      int usernumber = -1;
+
+        while (usernumber != 0)
+        {
+            
+         Console.WriteLine("ENTER a number, TYPE (O) TO QUIT. ");
+            string useranswer = Console.ReadLine();
+
+            usernumber = int.Parse(useranswer);
+
+           // se eu quiser adicionar a lista eu tenho que criar uma condicional
+
+           if (usernumber != 0)
+            {
+                numbers.Add(usernumber);
+            }
+        }
+             int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            Console.WriteLine($"The sum is {sum}");
+
+            float average = ((float)sum / numbers.Count);
+            Console.WriteLine($"The average is {average}");
+
+            int max = 0;
+            foreach(int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+            Console.WriteLine($"The max number is {max}"); 
+
+            int min = int.MaxValue;
+            foreach(int number in numbers)
+        {
+            if (number < min)
+            min = number;
+        }
+            Console.WriteLine($"The min number is {min}");
+
+
     }
+
 }

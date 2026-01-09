63fcb8bca50fe907958b1853bd2c02f9f8d86de6
Daniel Américo <danielrenatodanger@gmail.com>
2026-01-09 13:19:48 -0300

    finished exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) // FUNÇÃO QUE VAI CHAMAR TODAS AS OUTRAS QUE ESTÃO ABAIXO
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        displaywelcome();
+        string username = promptusername();
+        int usernumber = PromptUserNumber();
+
+        int squared = PromptySquareNumber(usernumber);      
+
+        displayResult(username, squared);
+    }
+    static void displaywelcome()
+    {
+        Console.WriteLine("Welcome to this task!!!");
+    }
+
+    static string promptusername()
+    {
+        Console.WriteLine("Enter your name please: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+        
+    }
+
+    static int PromptySquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void displayResult(string name, int square)
+    {
+        Console.WriteLine($"{name} your favorit number squared is {square}.");
     }
 }
+

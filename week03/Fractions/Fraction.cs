b15503a05b86114e828dc82caed0953ef22b5295
Daniel Américo <danielrenatodanger@gmail.com>
2026-01-28 16:58:31 -0300

    i remade this to learn better the content

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -2,19 +2,18 @@ using System;
 
 public class Fraction
 {
-    private int _top;
-    private int _bottom;
-
+    private int _top; // numerador
+    private int _bottom; // denominador
 
     public Fraction()
     {
-        _top = 1;
+        _top = 1; //como a atividade pediu se o usuario não passar nada a fração começa 1/1
         _bottom = 1;
     }
 
-    public Fraction(int wholeNumber)
-    {   
-        _top = wholeNumber;
+    public Fraction(int top)
+    {
+        _top = top;
         _bottom = 1;
     }
 
@@ -24,17 +23,17 @@ public class Fraction
         _bottom = bottom;
     }
 
-     public int GetTop() //public → qualquer lugar pode chamar, int → ele vai devolver um número, GetTop → nome que indica “pegar o numerador”
+    public int GetTop() // aqui usa para dizer o númerador 
     {
-        return _top; // 
+        return _top;
     }
 
-    public void SetTop(int top) // Por que isso é importante? Sem isso: ninguém consegue mudar o numerador depois de criado
+    public void SetTop(int top) // usa para alterar o valor
     {
         _top = top;
     }
 
-    public int GetBottom() // mesma logica do numerador
+    public int GetBottom()
     {
         return _bottom;
     }
@@ -49,8 +48,9 @@ public class Fraction
         return $"{_top}/{_bottom}";
     }
 
-    public double GetDecimalValue() // estamos usando double pois se nã usarmos e por exemplo fazer 3 dividido por 4 ele vai dar 0 pois o numero vai ser quebrado e INT não recebe numero quebrado 
+    public double GetDecimalValue() // o double permiti que façamos divisoes que o resultado seja números decimal já que int só aceita número cheio
     {
-        return(double)_top/_bottom;
+        return (double)_top / _bottom; // para fraçoes devemos usar o double e não o float
     }
+
 }
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,24 +1,21 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 
 public class Program
 {
     static void Main(string[] args)
     {
-        Fraction f1 = new Fraction(); //public Fraction()   _top = 1;     _bottom = 1;
-        Console.WriteLine(f1.GetFractionString());
-        Console.WriteLine(f1.GetDecimalValue());
+      Fraction f1 = new Fraction();//cada um desse é um construtor
+      Console.WriteLine(f1.GetFractionString());
+      Console.WriteLine(f1.GetDecimalValue());
 
-        Fraction f2 = new Fraction(5); // aqui testamos do file fraction.cs public Fraction(int wholeNumber)
-        Console.WriteLine(f2.GetFractionString());
-        Console.WriteLine(f2.GetDecimalValue());
+      Fraction f2 = new Fraction(5); // o c# entende qual class usar pelos parametros
+      Console.WriteLine(f2.GetFractionString());
+      Console.WriteLine(f2.GetDecimalValue());
 
-        Fraction f3 = new Fraction(3, 4); // aqui testamos  public Fraction(int top, int bottom)
-        Console.WriteLine(f3.GetFractionString());
-        Console.WriteLine(f3.GetDecimalValue());
-
-        Fraction f4 = new Fraction(1,3); // porque aqui usou dois parametros sendo que só tem uma função que tem dois parametros, a resposta foi essa, O C# escolhe QUAL construtor usar baseado nos parâmetros que você passa.
-        Console.WriteLine(f4.GetFractionString());
-        Console.WriteLine(f4.GetDecimalValue());
+      Fraction f3 = new Fraction(3, 4);
+      Console.WriteLine(f3.GetFractionString());
+      Console.WriteLine(f3.GetDecimalValue());
     }
 }
